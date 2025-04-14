0966397f7dfea363b619d617514b72be8cdb353c
felipegonzaleze <fgonech@gmail.com>
2025-04-14 19:29:53 -0400

    Ejercicio MiInformacionEnJava

---
+++ MiInformacionEnJava/src/MiInformacionEnJava.java
@@ -0,0 +1,11 @@
+
+public class MiInformacionEnJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hola, mi nombre es Felipe González.");
+		System.out.println("Tengo 21 años de edad.");
+		System.out.println("Soy de Chile y vivo en la ciudad de Quilpué.");
+		System.out.println("Me gusta mucho programar y los videojuegos.");
+	}
+
+}
